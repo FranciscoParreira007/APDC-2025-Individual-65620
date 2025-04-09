2bbd4531f0645c8d29504d70d1b03c1b48f3826e
FranciscoParreira65620 <f.parreira@campus.fct.unl.pt>
2025-04-09 16:08:51 +0100

    OP6 has been made. May need some alterations because of the attributes that may not be defined when a User is added.

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ListUsers.java
@@ -0,0 +1,301 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import com.google.cloud.datastore.*;
+import com.google.gson.Gson;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import pt.unl.fct.di.apdc.firstwebapp.enums.AccountProfile;
+import pt.unl.fct.di.apdc.firstwebapp.enums.AccountStatus;
+import pt.unl.fct.di.apdc.firstwebapp.enums.Roles;
+import pt.unl.fct.di.apdc.firstwebapp.util.ListUsersData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+
+@Path("/list")
+public class ListUsers {
+
+    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    private final Gson g = new Gson();
+
+    @POST
+    @Path("/listUsers")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listUsers(ListUsersData data) {
+
+        Transaction txn = datastore.newTransaction();
+
+        if (data == null || data.token.isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .build();
+        }
+
+        Key tokenKey = datastore.newKeyFactory()
+                .setKind("Token")
+                .newKey(data.token);
+        Entity tokenID = txn.get(tokenKey);
+
+        if (tokenID == null) {
+            txn.rollback();
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Requester not found.")
+                    .build();
+        }
+
+        String requesterRole = tokenID.getString("user_role");
+        List<Map<String, String>> users = new ArrayList<>();
+        Map<String, String> usersInfo = new HashMap<>();
+
+            switch (requesterRole) {
+                case "ADMIN" -> {
+                    Query<Entity> query = Query.newEntityQueryBuilder()
+                            .setKind("User").build();
+
+                    QueryResults<Entity> results = datastore.run(query);
+
+                    while (results.hasNext()) {
+                        Entity user = results.next();
+                        usersInfo = new HashMap<>();
+
+                        if (user.getString("user_name") == null) {
+                            usersInfo.put("user_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_name", user.getString("user_name"));
+                        }
+
+                        if (user.getString("user_email") == null) {
+                            usersInfo.put("user_email", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_email", user.getString("user_email"));
+                        }
+
+                        if (user.getString("user_complete_name") == null) {
+                            usersInfo.put("user_complete_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_complete_name", user.getString("user_complete_name"));
+                        }
+
+                        if (user.getString("user_account_profile") == null) {
+                            usersInfo.put("user_account_profile", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_account_profile", user.getString("user_account_profile"));
+                        }
+
+                        if (user.getString("user_account_status") == null) {
+                            usersInfo.put("user_account_status", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_account_status", user.getString("user_account_status"));
+                        }
+
+                        if (user.getString("user_address") == null) {
+                            usersInfo.put("user_address", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_address", user.getString("user_address"));
+                        }
+
+                        if (user.getString("user_cc_number") == null) {
+                            usersInfo.put("user_cc_number", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_cc_number", user.getString("user_cc_number"));
+                        }
+
+                        if (user.getString("user_nif") == null) {
+                            usersInfo.put("user_nif", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_nif", user.getString("user_nif"));
+                        }
+
+                        if (user.getString("user_nif_work_place") == null) {
+                            usersInfo.put("user_nif_work_place", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_nif_work_place", user.getString("user_nif_work_place"));
+                        }
+
+                        if (user.getString("user_phone") == null) {
+                            usersInfo.put("user_phone", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_phone", user.getString("user_phone"));
+                        }
+
+                        if (user.getString("user_pwd") == null) {
+                            usersInfo.put("user_pwd", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_pwd", user.getString("user_pwd"));
+                        }
+
+                        if (user.getString("user_role") == null) {
+                            usersInfo.put("user_role", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_role", user.getString("user_role"));
+                        }
+
+                        if (user.getString("user_work_function") == null) {
+                            usersInfo.put("user_work_function", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_work_function", user.getString("user_work_function"));
+                        }
+
+                        if (user.getString("user_work_place") == null) {
+                            usersInfo.put("user_work_place", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_work_place", user.getString("user_work_place"));
+                        }
+                        users.add(usersInfo);
+                    }
+                }
+
+                case "ENDUSER" -> {
+                    Query<Entity> query = Query.newEntityQueryBuilder()
+                            .setKind("User")
+                            .setFilter(StructuredQuery.PropertyFilter.eq("user_role", Roles.ENDUSER.getDescription()))
+                            .setFilter(StructuredQuery.PropertyFilter.eq("user_account_status", AccountStatus.ACTIVE.getDescription()))
+                            .setFilter(StructuredQuery.PropertyFilter.eq("user_account_profile", AccountProfile.PUBLIC.getDescription()))
+                            .build();
+                    QueryResults<Entity> results = datastore.run(query);
+
+                    while (results.hasNext()) {
+                        Entity user = results.next();
+                        usersInfo = new HashMap<>();
+
+                        if(user.getString("user_name") == null) {
+                            usersInfo.put("user_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_name", user.getString("user_name"));
+                        }
+                        if (user.getString("user_email") == null) {
+                            usersInfo.put("user_email", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_email", user.getString("user_email"));
+                        }
+
+                        if (user.getString("user_complete_name") == null) {
+                            usersInfo.put("user_complete_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_complete_name", user.getString("user_complete_name"));
+                        }
+                        users.add(usersInfo);
+                    }
+                }
+                case "BACKOFFICE" -> {
+
+                    Query<Entity> query = Query.newEntityQueryBuilder()
+                            .setKind("User")
+                            .setFilter(StructuredQuery.PropertyFilter.eq("user_role", Roles.ENDUSER.getDescription()))
+                            .build();
+                    QueryResults<Entity> results = datastore.run(query);
+
+                    while (results.hasNext()) {
+                        Entity user = results.next();
+                        usersInfo = new HashMap<>();
+
+                        if (user.getString("user_name") == null) {
+                            usersInfo.put("user_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_name", user.getString("user_name"));
+                        }
+
+                        if (user.getString("user_email") == null) {
+                            usersInfo.put("user_email", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_email", user.getString("user_email"));
+                        }
+
+                        if (user.getString("user_complete_name") == null) {
+                            usersInfo.put("user_complete_name", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_complete_name", user.getString("user_complete_name"));
+                        }
+
+                        if (user.getString("user_account_profile") == null) {
+                            usersInfo.put("user_account_profile", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_account_profile", user.getString("user_account_profile"));
+                        }
+
+                        if (user.getString("user_account_status") == null) {
+                            usersInfo.put("user_account_status", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_account_status", user.getString("user_account_status"));
+                        }
+
+                        if (user.getString("user_address") == null) {
+                            usersInfo.put("user_address", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_address", user.getString("user_address"));
+                        }
+
+                        if (user.getString("user_cc_number") == null) {
+                            usersInfo.put("user_cc_number", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_cc_number", user.getString("user_cc_number"));
+                        }
+
+                        if (user.getString("user_nif") == null) {
+                            usersInfo.put("user_nif", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_nif", user.getString("user_nif"));
+                        }
+
+                        if (user.getString("user_nif_work_place") == null) {
+                            usersInfo.put("user_nif_work_place", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_nif_work_place", user.getString("user_nif_work_place"));
+                        }
+
+                        if (user.getString("user_phone") == null) {
+                            usersInfo.put("user_phone", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_phone", user.getString("user_phone"));
+                        }
+
+                        if (user.getString("user_pwd") == null) {
+                            usersInfo.put("user_pwd", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_pwd", user.getString("user_pwd"));
+                        }
+
+                        if (user.getString("user_role") == null) {
+                            usersInfo.put("user_role", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_role", user.getString("user_role"));
+                        }
+
+                        if (user.getString("user_work_function") == null) {
+                            usersInfo.put("user_work_function", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_work_function", user.getString("user_work_function"));
+                        }
+
+                        if (user.getString("user_work_place") == null) {
+                            usersInfo.put("user_work_place", "NOT DEFINED");
+                        } else {
+                            usersInfo.put("user_work_place", user.getString("user_work_place"));
+                        }
+
+                        users.add(usersInfo);
+                    }
+                }
+
+                default -> {
+                    txn.rollback();
+                    return Response.status(Status.BAD_REQUEST)
+                            .build();
+                }
+            }
+            txn.commit();
+
+        return Response.ok(g.toJson(users)).build();
+
+    }
+}
---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/ListUsersData.java
@@ -0,0 +1,13 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+public class ListUsersData {
+
+    public String token;
+
+    public ListUsersData(){}
+
+    public ListUsersData(String token){
+        this.token = token;
+    }
+
+}
